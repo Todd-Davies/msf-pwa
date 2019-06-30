5e88926517dbe536974ac6e6a90a7e98cac6ee5d
Todd Davies <todd434@gmail.com>
2019-06-30 11:30:19 +0200

    Add simple search field

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const ID_CONTENT = 'content';
+const ID_SEARCH = 'search-box';
 var serviceWorker = null;
+var listJson = null;
 
 function handleServiceWorkerActive(registration) {
     console.log('Service worker registered.', registration);
@@ -15,7 +17,51 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const renderList = function() {
+const renderList = function(json) {
+  // Create a list
+  const ul = document.createElement("ul");
+
+  // Iterate over the list and add new elements
+  json.forEach(article => {
+    const li = document.createElement("li");
+    li.classList.add("articleItem");
+
+    // Create the article title
+    const articleTitle = document.createElement("a");
+    articleTitle.appendChild(document.createTextNode(article.name));
+    articleTitle.classList.add("articleTitle");
+    articleTitle.setAttribute("href", article.file);
+    articleTitle.setAttribute("target", "_blank");
+    li.appendChild(articleTitle);
+
+    // Create the author list
+    const authors = document.createElement("span");
+    authors.appendChild(document.createTextNode(article.authors.join(", ")));
+    authors.classList.add("authors");
+    li.appendChild(authors);
+
+    // Create the author list
+    const abstract = document.createElement("span");
+    abstract.innerHTML = article.abstract.replace(/\n/g,"<br>");
+    abstract.classList.add("abstract");
+    li.appendChild(abstract);
+
+    ul.appendChild(li);
+  });
+
+  const card = document.getElementById(ID_CONTENT);
+  card.innerHTML = '';
+  card.appendChild(ul);
+};
+
+
+const search = function() {
+  const search = document.getElementById(ID_SEARCH);
+  const term = search.value;
+  renderList(listJson.filter(article => article.name.toLowerCase().includes(term)));
+}
+
+const initialise = function() {
   // Fetch the list
   fetch('api/list.json')
   .then(response => {
@@ -25,45 +71,10 @@ const renderList = function() {
 
     return response.json();
   }).then(json => {
-    // Create a list
-    const ul = document.createElement("ul");
-
-    // Iterate over the list and add new elements
-    json.forEach(article => {
-      const li = document.createElement("li");
-      li.classList.add("articleItem");
-
-      // Create the article title
-      const articleTitle = document.createElement("a");
-      articleTitle.appendChild(document.createTextNode(article.name));
-      articleTitle.classList.add("articleTitle");
-      articleTitle.setAttribute("href", article.file);
-      articleTitle.setAttribute("target", "_blank");
-      li.appendChild(articleTitle);
-
-      // Create the author list
-      const authors = document.createElement("span");
-      authors.appendChild(document.createTextNode(article.authors.join(", ")));
-      authors.classList.add("authors");
-      li.appendChild(authors);
-      
-      // Create the author list
-      const abstract = document.createElement("span");
-      abstract.innerHTML = article.abstract.replace(/\n/g,"<br>");
-      abstract.classList.add("abstract");
-      li.appendChild(abstract);
-
-      ul.appendChild(li);
-    });
-
-    const card = document.getElementById(ID_CONTENT);
-    card.innerHTML = '';
-    card.appendChild(ul);
-  });
+    listJson = json;
+    return json;
+  }).then(renderList);
 };
 
-const saveOffline = function(articlePath) {
-  serviceWorker.postMessage(articlePath);
-}
 
-window.onload = renderList;
+window.onload = initialise;
